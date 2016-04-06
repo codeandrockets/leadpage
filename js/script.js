3becfd3250eb9fa482b36674240ec99bf41f6bc5
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-06 20:55:04 +1200

    Add rotating background

--- js/script.js
+++ js/script.js
@@ -21,3 +21,26 @@ $(document).ready(function() {
     });
     
 });
+
+$(window).load(function(){
+$('img.bgfade').hide();
+function anim() {
+    $("#wrap img.bgfade").first().appendTo('#wrap').fadeOut(1500);
+    $("#wrap img").first().fadeIn(1500);
+    setTimeout(anim, 3000);
+
+
+
+}
+anim();})
+$(window).resize(function(){window.location.href=window.location.href});
+
+
+var terms = ["Xplore", "Discover", "Find", "Tresure"];
+
+function rotateTerm() {
+  var ct = $("#rotate").data("term") || 0;
+  $("#rotate").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
+              .delay(2800).fadeOut(200, rotateTerm);
+}
+$(rotateTerm);
